72b12884c2c6e147f5b302f5ed39a36042cf1c3b
Choosla <cnckddn0146@gmail.com>
2025-08-27 12:30:42 +0900

    [auto] 2025-08-27 12:30:42 +0900 (repo: iOS, branch: main, base: 31fcf42, author: Choosla) files: total 9 (A:4 M:5 D:0 R:0 C:0) | lines: +149 -14 changes:  - 2Image/2Image.xcodeproj/project.xcworkspace/xcuserdata/admin.xcuserdatad/UserInterfaceState.xcuserstate  - Clazzi/Clazzi.xcodeproj/project.xcworkspace/xcuserdata/admin.xcuserdatad/UserInterfaceState.xcuserstate  - Clazzi/Clazzi/View/VoteListView.swift  - FlipFlick_iOS/FlipFlick_iOS.xcodeproj/project.xcworkspace/xcuserdata/admin.xcuserdatad/UserInterfaceState.xcuserstate  - FlipFlick_iOS/FlipFlick_iOS/Assets.xcassets/logo.imageset/Contents.json  - FlipFlick_iOS/FlipFlick_iOS/Assets.xcassets/logo.imageset/logo.png  - FlipFlick_iOS/FlipFlick_iOS/Assets.xcassets/logo_full.imageset/Contents.json  - FlipFlick_iOS/FlipFlick_iOS/Assets.xcassets/logo_full.imageset/logo_full.png  - FlipFlick_iOS/FlipFlick_iOS/View/ContentView.swift

--- Clazzi/Clazzi/View/VoteListView.swift
+++ Clazzi/Clazzi/View/VoteListView.swift
@@ -8,23 +8,56 @@
 import SwiftUI
 
 struct VoteListView: View {
-//  let votes = ["첫 번째 투표","두 번째 투표", "세 번째 투표"]
+  //  let votes = ["첫 번째 투표","두 번째 투표", "세 번째 투표"]
   @State var votes = [
     Vote(title: "오늘 점심 메뉴 추천", options: ["돈까스","짜장"]),
     Vote(title: "오늘 저녁 메뉴 추천", options: ["야시장","편의점","굶기"]),
     Vote(title: "오늘 칼바람 추천", options: ["동진","박짜장","추이가"]),
   ]
+  
+  
+  // 투표 생성관련
   @State private var isPresentingCreate = false
+  
+  // 투표 삭제 관련
+  @State private var showDeleteAlert: Bool = false
+  @State private var voteToDelete: Vote? = nil
+  
+  // 투표 수정 관련
+  @State private var isPresentingEdit = false
+  
   var body: some View {
+    //    List{
+    //      LazyVStack(spacing: 16){
+    //        ForEach(votes.indices){ index in
+    //          let vote = votes[index]
+    //          NavigationLink(destination: VoteView(vote : vote)) {
+    //            VoteCardView(vote: vote)
+    //          }
+    //        }
+    //        .onDelete { indexSet in
+    //          votes.remove(atOffsets: indexSet)
+    //        }
+    //      }
+    //    }.listStyle(.grouped)
+    
     NavigationStack {
       ZStack {
         ScrollView{
           LazyVStack(spacing: 16){
-            ForEach(votes){ vote in
+            ForEach(votes.indices, id: \.self){ index in
+              let vote = votes[index]
               NavigationLink(destination: VoteView(vote : vote)) {
-                VoteCardView(vote: vote)
+                VoteCardView(vote: vote){
+                  voteToDelete = vote
+                  showDeleteAlert = true
+                  //                  votes.remove(at: index)
+                } onEdit: {
+                  isPresentingEdit = true
+                }
               }
             }
+            
           }.padding()
         }
         VStack{
@@ -62,16 +95,42 @@ struct VoteListView: View {
             }
           }
         }
-      //      // 화면 이동 방법 2: 상태를 이용한 이동 방법
-      //      .navigationDestination(isPresented: $isPresentingCreate) {
-      //        CreateVoteView()
-      //      }
-      
-        .sheet(isPresented: $isPresentingCreate) {
+      // 화면 이동 방법 2: 상태를 이용한 이동 방법
+        .navigationDestination(isPresented: $isPresentingCreate) {
           CreateVoteView(){ vote in
             votes.append(vote)
           }
         }
+      
+      // 수정화면
+        .navigationDestination(isPresented: $isPresentingEdit) {
+          CreateVoteView(){ vote in
+            votes.append(vote)
+          }
+        }
+      
+      //        // 모달(바텀 시트)를 활용한 화면 화면 띄우는 방법(상태 이용)
+      //        .sheet(isPresented: $isPresentingCreate) {
+      //          CreateVoteView(){ vote in
+      //            votes.append(vote)
+      //          }
+      //        }
+      
+      // 삭제 ㅏㄹ러트
+        .alert("투표를 삭제하시겠습니까?",isPresented: $showDeleteAlert){
+          Button("삭제", role : .destructive){
+            if let target = voteToDelete, let index = votes.firstIndex(where: {$0.id == target.id}){
+              votes.remove(at: index)
+            }
+          }
+          Button("취소",role: .cancel){}
+        } message: {
+          if let target = voteToDelete{
+            Text("'\(target.title)'가 삭제됩니다.")
+          }
+        }
+      
+      
       
     }
   }
@@ -79,12 +138,23 @@ struct VoteListView: View {
 
 struct VoteCardView: View {
   let vote: Vote
+  let onDelete: () -> Void
+  let onEdit: () -> Void
   var body: some View {
-    VStack(alignment: .leading) {
-      Text(vote.title)
-        .font(.headline)
-      Text("투표항목 보기")
-        .font(.subheadline)
+    HStack(alignment: .top) {
+      VStack(alignment: .leading) {
+        Text(vote.title)
+          .font(.headline)
+        Text("투표항목 보기")
+          .font(.subheadline)
+      }
+      Spacer()
+      Button(action: { onEdit() }) {
+        Image(systemName: "pencil")
+      }
+      Button(action: { onDelete() }) {
+        Image(systemName: "trash")
+      }
     }
     .padding(16)
     .frame(maxWidth: .infinity, alignment: .leading)
--- FlipFlick_iOS/FlipFlick_iOS/View/ContentView.swift
+++ FlipFlick_iOS/FlipFlick_iOS/View/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-      
+      NavigationStack{
+        ScrollView {
+          VStack{
+            MovieCard()
+            MovieCard()
+          }
+//          .toolbar {
+//            ToolbarItem(placement: .principal) {
+//              HStack {
+//                Image("logo_full")
+//                  .resizable()
+//                  .aspectRatio(contentMode: .fit)
+//              }.padding(.bottom, 10)
+//            }
+//          }
+          .navigationTitle("볼래말래")
+        }
+      }
     }
+}
+
+struct MovieCard: View{
+  var body: some View {
+    Text("Hello, World!")
+  }
 }
 
 #Preview {
